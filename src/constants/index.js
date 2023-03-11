496edcb9039e7e1efeca931d64cb36c45e986be1
Hester Lai <h.lai52@gmail.com>
2023-03-10 18:10:51 -0800

    Added a new word to test

--- src/constants/index.js
+++ src/constants/index.js
@@ -117,7 +117,7 @@ const technologies = [
 const experiences = [
   {
     title: "React.js Developer",
-    company_name: "Starbucks",
+    company_name: "Starbucks Zone",
     icon: starbucks,
     iconBg: "#383E56",
     date: "March 2020 - April 2021",
