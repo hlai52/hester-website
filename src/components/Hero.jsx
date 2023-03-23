d2a0104ed4f29f2332384bfc14ea297e103efcd1
Hester Lai <h.lai52@gmail.com>
2023-03-22 18:38:09 -0700

    Added some divs and created some visuals

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,9 +11,18 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Hesty </span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I'm a developer from <br className="sm:block hidden" /> Canada.
+          </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   );
 };
