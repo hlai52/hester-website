181037651477db8ef0cc2b6bb06afa31d8f4c22b
Hester Lai <h.lai52@gmail.com>
2023-03-29 16:08:43 -0700

    Moved the navbar to the right and added a button below the computer image to have it lead to the about section later

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,6 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
-      hero
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
@@ -23,6 +22,23 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute bottom-32 xs:bottom-8 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeateType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
