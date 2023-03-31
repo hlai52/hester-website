82a614f350194e67b2e1fd09b04780d2c6aa1b0c
Hester Lai <h.lai52@gmail.com>
2023-03-31 16:07:15 -0700

    Creating an about section and Introduction and Overview

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,40 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{title}</p>;
+};
+
 const About = () => {
-  return <div>3D Developer Portfolio </div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introducton</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer... with experience in Javascript and
+        frameworks such as React, Node.js. Let's work together to bring your
+        ideas to life and make them a reality! blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
