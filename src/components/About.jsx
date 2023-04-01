0bcf9afe08ae2978f91f8e9e2bba53e84bb55e89
Hester Lai <h.lai52@gmail.com>
2023-03-31 17:23:07 -0700

    Creating cards layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,14 @@ import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => {
-  return <p>{title}</p>;
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      ></motion.div>
+    </Tilt>
+  );
 };
 
 const About = () => {
