3665cf3b20058b68fdd3b473fb7ca2ec180faa0a
Hester Lai <h.lai52@gmail.com>
2023-04-03 16:54:57 -0700

    Finished the section wrapper

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 
 const SectionWrapper = (Component, idName) => {
-  return <div>SectionWrapper</div>;
+  function HOC() {
+    return (
+      <motion.section>
+        <Component />
+      </motion.section>
+    );
+  }
 };
 
 export default SectionWrapper;
