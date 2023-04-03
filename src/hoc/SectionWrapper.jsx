4a6d22e374104f78724c5e2c5aa6b55680eb3d75
Hester Lai <h.lai52@gmail.com>
2023-04-03 16:12:49 -0700

    Creating a section wrapper

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { styles } from "../styles";
+import { staggerContainer } from "../utils/motion";
+
+const SectionWrapper = (Component, idName) => {
+  return <div>SectionWrapper</div>;
+};
+
+export default SectionWrapper;
