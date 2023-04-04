001fed1a1a1b0585f346807b5bad9ef60ea8533b
Hester Lai <h.lai52@gmail.com>
2023-04-04 16:19:14 -0700

    Creating an Experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p>hello</p>
+      </motion.div>
+    </>
+  );
+};
 
-export default Experience
+export default Experience;
