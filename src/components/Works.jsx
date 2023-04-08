b06a7f62504bd69aaf729173a3f43afd7efbe98f
Hester Lai <h.lai52@gmail.com>
2023-04-07 18:54:11 -0700

    Set up the project section with each section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,8 +6,46 @@ import { github } from "../assets";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
-const Works = () => {
-  return <div>Works</div>;
+
+const ProjectCard = (
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link
+) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5)}>test</motion.div>
+  );
 };
 
-export default Works;
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}> Projects</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Following project showcases my skills and examples. It reflects my
+          ability to solve complex problems, learn and work with different
+          technologies and manage projects effectively.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
