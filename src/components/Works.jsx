e8b73a4294bfcea2410cb16fe1c7619d016dc26e
Hester Lai <h.lai52@gmail.com>
2023-04-11 16:50:07 -0700

    Added the title and description to the cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -44,9 +44,9 @@ const ProjectCard = ({
             </div>
           </div>
         </div>
-        <div>
-          <h3>{name}</h3>
-          <p>{description}</p>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text=[14px]">{description}</p>
         </div>
       </Tilt>
     </motion.div>
