7b919f16c8867fd5157e599eaa1d252c7b3672d7
Hester Lai <h.lai52@gmail.com>
2023-04-18 18:18:57 -0700

    Set up the layout for a form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,23 +35,63 @@ const Contact = () => {
           className="mt-12 flex flex-col gap-8"
         >
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
+            <span className="text-white font-medium mb-4">Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary
+            py-4
+            px-6
+            
+            placeholder:text-secondary
+            text-white rounded-lg outlined-none border-none font-medium
+            
+            "
+            />
           </label>
-          <input
-            type="text"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-            placeholder="What's your name?"
-            className="bg-tertiary
-             py-4
-             px-6
 
-             placeholder:text-secondary
-             text-white rounded-lg outlined-none border-none font-medium
-             
-              "
-          />
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary
+            py-4
+            px-6
+            
+            placeholder:text-secondary
+            text-white rounded-lg outlined-none border-none font-medium
+            
+            "
+            />
+          </label>
+
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Message</span>
+            <textarea
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What would you like to share?"
+              className="bg-tertiary
+            py-4
+            px-6
+            
+            placeholder:text-secondary
+            text-white rounded-lg outlined-none border-none font-medium
+            
+            "
+            />
+          </label>
+
+          <button type="submit">{loading ? "Sending..." : "Send"}</button>
         </form>
       </motion.div>
     </div>
