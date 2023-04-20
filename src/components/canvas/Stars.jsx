9b90d21d65ea7b0edb6c21554f9ffccad5faaea4
Hester Lai <h.lai52@gmail.com>
2023-04-20 15:15:05 -0700

    Updated the stars with the position

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -4,7 +4,15 @@ import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
-  return <div>Stars</div>;
+  const ref = useRef();
+
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+
+  return (
+    <group>
+      <Points ref={ref} positions={sphere} stride={3} />
+    </group>
+  );
 };
 
 const StarCanvas = () => {
@@ -14,6 +22,8 @@ const StarCanvas = () => {
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
+
+        <Preload all />
       </Canvas>
     </div>
   );
