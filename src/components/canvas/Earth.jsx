0d8e21ff03326738b99911792194ee14b6c845da
Hester Lai <h.lai52@gmail.com>
2023-04-19 17:52:43 -0700

    Updated canvas with animation base

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,28 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return <div>Earth</div>;
+  return <div>Earth hello</div>;
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameLoop="demand"
+      gl={{ preserverDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  );
 };
 
 export default Earth;
