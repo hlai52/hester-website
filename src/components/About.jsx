dff0372198175a5f771375cd0fe8bfaaac35c6ee
Hester Lai <h.lai52@gmail.com>
2023-05-04 15:25:26 -0700

    Testing relative path

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         <div className="about__container">
-          <img className="profile-pic" src="../src/assets/HL.png"></img>
+          <img className="profile-pic" src="./../src/assets/HL.png"></img>
           I'm a software developer with experience in Javascript and frameworks
           such as React and Node.js. Let's work together to bring your ideas to
           life and make them a reality.
