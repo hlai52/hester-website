8b253c6b6d821a9da1c1716518cad443945b7a7e
Hester Lai <h.lai52@gmail.com>
2023-05-05 18:25:03 -0700

    Update text for techs

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,14 +1,15 @@
 import { BallCanvas } from "./canvas";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { technologies } from "../constants";
+import "./tech.css";
 
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
+        <div className="w-28 h-28 tech" key={technology.name}>
           <BallCanvas icon={technology.icon} />
-          {technology.name}
+          <p className="tech-name">{technology.name}</p>
         </div>
       ))}
     </div>
