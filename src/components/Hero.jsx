026b1284126d1b3fa34612328e4c1314ac9fb42a
Hester Lai <h.lai52@gmail.com>
2023-05-11 16:07:15 -0700

    Changing the animation on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,14 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto  .d-none .d-sm-block ">
+      {/* //sm:hidden */}
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5  `}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        <div className="flex flex-col justify-center items-center mt-5 ">
+          <div className="w-5 h-5 rounded-full bg-[#915eff] " />
+          <div className="w-1 sm:h-80 h-40 violet-gradient " />
         </div>
 
         <div>
@@ -44,7 +45,7 @@ const Hero = () => {
             <div></div>
           </div>
         </a>
-        <div> ← </div>
+        <div> </div>
       </div>
     </section>
   );
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -65,6 +65,7 @@ const ComputerCanvas = () => {
             maxPolarAngle={Math.PI / 2}
             minPolarAngle={Math.PI / 2}
           />
+
           <Computers />
         </Suspense>
 
