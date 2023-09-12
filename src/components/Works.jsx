a73623306cd87d67b4426bc667aa4f3fe8605fc7
Hester L <109488298+hlai52@users.noreply.github.com>
2023-09-11 23:03:19 -0700

    Added a line break

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -77,6 +77,7 @@ const Works = () => {
           Following project showcases my skills and examples. It reflects my
           ability to solve complex problems, learn and work with different
           technologies and manage projects effectively.
+          <br></br>
           <div>
             <p>
               Click the link below for my
