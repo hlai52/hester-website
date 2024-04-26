6cc2c4f16c876fa85570c00bf4310ff8029e8b77
Hester L <109488298+hlai52@users.noreply.github.com>
2024-04-25 23:21:00 -0700

    ~~Anonymous~~

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Hello Translink team! <br className="sm:block hidden" /> Welcome
-            Amrit Brar, Robyn Reekie & Bobak Zojaji.
+            Amrit, Robyn & Bobak.
           </p>
         </div>
       </div>
